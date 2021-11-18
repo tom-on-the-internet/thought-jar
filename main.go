b8ebea4a98c708b8c551b91005bd85d98448335e
Thomas Steven <thomaspatricksteven@gmail.com>
2021-11-17 21:50:38 -0500

    use web server

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"log"
 	"math/rand"
+	"net/http"
+	"os"
 	"strings"
 	"time"
 )
@@ -15,6 +18,36 @@ var thoughtBytes []byte
 var jarBytes []byte
 
 func main() {
+	if isWeb() {
+		serveWeb()
+	} else {
+		print()
+	}
+}
+
+// Determines if the user wants this to operates as a web server
+// or a one time print.
+func isWeb() bool {
+	return len(os.Args) == 2 && os.Args[1] == "web"
+}
+
+// Serves web requests asking for a thought.
+func serveWeb() {
+	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+		fmt.Fprintln(w, thought())
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// Prints a thought to stdout.
+func print() {
+	fmt.Println(thought())
+}
+
+// Gets a single thought from the jar.
+func thought() string {
+	image := string(jarBytes)
 	text := string(thoughtBytes)
 
 	thoughts := strings.Split(text, "\n--\n")
@@ -22,7 +55,5 @@ func main() {
 	rand.Seed(time.Now().Unix()) // initialize global pseudo random generator
 	randomIndex := rand.Intn(len(thoughts))
 
-	message := string(jarBytes) + thoughts[randomIndex]
-
-	fmt.Println(message)
+	return image + thoughts[randomIndex]
 }
